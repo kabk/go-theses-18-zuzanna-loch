29aafc9a40385cff379a5a9f48d765d699430d37
zuzannaloch <zuzannamarialoch@gmail.com>
2018-01-10 18:41:02 +0100

    duze zmiany

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function myFunction(){
   var element = document.getElementById("video");
-  var desiredPosition = 1800;
+  var desiredPosition = 1900;
   if(window.pageYOffset >= desiredPosition){
      element.style.position = "absolute";
-     element.style.top = "1500px";
+     element.style.top = "1690px";
     }
   else {
 	  element.style.top = "-200px";
