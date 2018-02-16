e7580051a778e0099f3640f9a5d2d4d56d9d81c6
zuzannaloch <zuzannamarialoch@gmail.com>
2018-02-16 15:26:52 +0100

    siemaneczko

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function myFunction(){
   var element = document.getElementById("video");
   var komunikatwait = document.getElementById("wait");
-  var desiredPosition = 1900;
+  var desiredPosition = 1950;
+  var wh = $(window).height();
+
   if(window.pageYOffset >= desiredPosition){
      element.style.position = "absolute";
-     element.style.top = "1690px";
+     element.style.top = desiredPosition+"px"; //1690px
      komunikatwait.style.display = "none";
     }
   else {
-	  element.style.top = "-200px";
+	  element.style.top = "0px";
      element.style.position = "fixed"; 
      komunikatwait.style.display = "inline"; 
    } 
